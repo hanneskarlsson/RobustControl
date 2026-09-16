e39511204ccd435d8f5a4ac2d62b8df020093b65
Hannes Karlsson <hanneskachalmers.se>
2026-09-16 21:27:15 +0200

    removed minreal()

--- A1/EEN050_Assignment_1_2_and_3_template.m
+++ A1/EEN050_Assignment_1_2_and_3_template.m
@@ -312,10 +312,7 @@ Wu.OutputName = 'zu';
 [usys_gt, info_gt] = ucover(GTu_samples, GT, 2, [], 'OutputMult');
 [usys_ge, info_ge] = ucover(Geu_samples, Ge, 2, [], 'OutputMult');
 
-WmT = minreal(info_gt.W1);
-Wme = minreal(info_ge.W1);
-
-Wm = ss(blkdiag(WmT,Wme));
+Wm = ss(blkdiag(info_gt.W1,info_ge.W1));
 Wm.InputName = 'udelta';
 Wm.OutputName = 'Wm';
 % Provide appropriate input/output names
